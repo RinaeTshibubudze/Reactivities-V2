6c3585ac2e2cea03c36c080f3ffd7781e3ac231a
Rinae Tshibubudze <vharhee@gmail.com>
2025-04-07 12:06:00 +0200

    ignore files

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -15,7 +15,7 @@ public class ActivitiesController(AppDbContext context) : BaseApiController
     }
 
     [HttpGet("{id}")]
-    public async Task<ActionResult<Activity>> GetActivityDetail(Guid id)
+    public async Task<ActionResult<Activity>> GetActivityDetail(string id)
     {
         var activity = await context.Activities.FindAsync(id);
         if (activity == null) return NotFound();
